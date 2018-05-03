519f93ba49a3edd73db8c898ccaf6d60883b90f0
champInteraction <38883145+champInteraction@users.noreply.github.com>
2018-05-02 23:50:00 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,165 @@
+<!DOCTYPE HTML>
+<!--
+	Typify by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<title>
+		  BG's Engine Clinic : Putting the POWER back in Power Equipment
+		  </title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/styles.css">
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body>
+<!-- logo and menu -->
+
+		<!-- Banner -->
+			<section id="banner">
+			<div id='cssmenu'>
+				<img alt="BG's Engine Clinic" src="logo.png" style="padding-left:4em;padding-right:6em;">
+				<ul>
+				   <li class='active'><a href='#top'>HOME</a></li>
+				   <li><a href='#two'>ABOUT US</a></li>
+				   <li><a href='#three'>HOW TO FIND US</a></li>
+				</ul>
+				</div>
+				<!--<h2><strong>Welcome to BG's Engine Clinic</strong> </h2>
+				<p>Welcome to BG's Engine Clinic, where we help to put the POWER back in POWER EQUIPMENT. 
+Our local customers can browse our informational pages, business hours, and Contact us by email for quicker service, in addition to shopping our parts inventory, and pick up their orders in store to save waiting time. </p>
+				<ul class="actions">
+					<li><a href="#" class="button special">Get started</a></li>
+				</ul>-->
+				<div class="">Latest Products</div>
+      <div class="">
+        <div class="">
+          <div class=""><a href="#">09729301 Forced Air Heater fan (Salamander/torpedo heater) 155K BTU & up 8-1/4</a></div>
+          <div class=""><a href="#"><img src="images/img1.jpg" alt="" border="0" /></a></div>
+          <div class=""><span class="">$16.77</span></div>
+        </div>
+        <div class=""> <a href="#" class="prod_buy">Add to Cart</a> <a href="8-1/4 diameter 5-Blade fan (5/16 shaft) includes set screw.
+Fits larger barrel heaters from 155,000 BTU and up, such as Reddy R155, etc."	class="">Details</a> </div>
+      </div>
+	  
+			</section>
+
+		<!-- One -->
+		<!--	<section id="one" class="wrapper special">
+				<div class="inner">
+					<header class="major">
+						<h2>Lorem ipsum feugiat tempus</h2>
+					</header>
+					<div class="features">
+						<div class="feature">
+							<i class="fa fa-diamond"></i>
+							<h3>Etiam consequat</h3>
+							<p>Sed tempus feugiat adipiscing nisl cursus aliquam dolore.</p>
+						</div>
+						<div class="feature">
+							<i class="fa fa-copy"></i>
+							<h3>Sed adipiscing</h3>
+							<p>Sed tempus feugiat adipiscing nisl cursus aliquam dolore.</p>
+						</div>
+						<div class="feature">
+							<i class="fa fa-paper-plane-o"></i>
+							<h3>Feugiat nullam</h3>
+							<p>Sed tempus feugiat adipiscing nisl cursus aliquam dolore.</p>
+						</div>
+						<div class="feature">
+							<i class="fa fa-save"></i>
+							<h3>Amet imperdiet</h3>
+							<p>Sed tempus feugiat adipiscing nisl cursus aliquam dolore.</p>
+						</div>
+						<div class="feature">
+							<i class="fa fa-envelope-o"></i>
+							<h3>Tempus lorem</h3>
+							<p>Sed tempus feugiat adipiscing nisl cursus aliquam dolore.</p>
+						</div>
+					</div>
+				</div>
+			</section>-->
+			
+			<!-- Two -->
+			<section id="two" class="wrapper special">
+				<div class="inner">
+					<header class="major">
+						<h2>ABOUT US</h2>
+					</header>
+					<div class="features">
+						<div class="about">
+							<i class="fa fa-diamond"></i>
+							<h3>First impressions are very important! That is why we want you to know all about BG's Engine Clinic!</h3>
+							<p>With 30 years' experience in Outdoor Power Equipment Service and Repair, we bring a long tradition of quality repair service to your lawn & garden equipment.</p>
+<p>Brian started working on lawn mowers at the age of 12, and a couple years later, established BG's Engine Clinic (1983) while still in High School. We've been working with Power Equipment ever since!</p>
+<p>Since then, Brian has been an ASE Certified Triple Master mechanic (Master Truck, Master Auto, Master Machinist, with Advanced L1 and L2 electronics certifications), Ford Factory trained Technician (nearly achieved Ford Master Engine Technician), Heavy Equipment and Heavy truck mechanic, PA State certified Inspection Mechanic, and most recently, a 5 year stint as a programmer analyst and Linux systems administrator for a Web-Hosting company in Miami, Florida. 
+However, our heart has always been in Lawn & Garden service and repairs, so, We are back and ready to build an even better small engine shop than we had before!</p>
+<p>We are currently an Authorized servicing dealer for Echo, Shindaiwa, BearCat, Kero-World wicks, parts & accessories, Desa barrel heaters, Mr, Heater heaters, Jiffy Ice Drills, Badger Earth Drills, Rapala Strikemaster Ice Augers, Briggs & Stratton products, Kohler, Tecumseh, Honda Engines, MTD Products including Columbia, Troy-Bilt, Yardman, Yard Machines and White Outdoor brands.</p>
+<p>We also carry B3C Fuel Solutions products and highly recommend their use to our customers.</p>
+<p>We sell our products on many sales venues, including here, Amazon and on eBay, with thousands of products in stock and ready to ship.</p>
+<p>For our local customers, Simply contact us and we are ready to meet your repair needs.
+Phone: 814-544-7558</p>
+
+<p>Or, E-Mail us - 24 hours a day, 7 days a week - Just click the Contact Us button at the bottom right of any page
+We usually respond to email within hours, but may be up to 24 hours to reply.</p>
+ 
+<p>For Help, Advice, or just to Network with other Outdoor Power Equipment Technicians, Visit the Professional Power Equipment Technicians' and Education Network (PPETEN.COM)
+Our logo was concieved and designed by Brian Gustin, and the final graphic rendering was done by Exxia.
+All website content, the design, and logo are copyright © 2010-2014 by Brian Gustin, D/B/A BG's Engine Clinic. 
+All Rights Reserved. All other designated trademarks, logos, and brands are the property of their respective owners.</p>
+						</div>
+					</div>
+				</div>
+			</section>
+
+		<!-- Two -->
+			<section id="three" class="wrapper style2 special">
+				<div>
+					<header>
+						<h2>HOW TO FIND US</h2>
+					</header>
+					<div  style="text-align:left;padding-left:6em;padding-right:6em;">
+					<h2>Convenient location at the corner of Main & Pomeroy St. in Roulette, PA</h2>
+					<p>From Coudersport: Take U.S. Route 6 West to Roulette. </p>
+<p>If taking first left into Roulette: travel west on Main Street through town, past the post office and town park, Shop will be on your left before the second 4-way intersection. </p>
+<p>If taking the Second left into Roulette (just before Close's lumber and the Baseball diamond), travel south on Fishing Creek Extension to the stop sign, turn left and shop is immediately on the right. </p>
+
+<p>From Port Allegany: Take U.S. Route 6 East to Roulette, Take the SECOND right turn into town, just past Close's Lumber and the Baseball diamond, follow Fishing Creek Extension south to the stop sign, turn left, shop is immediately on the right.</p>
+				</div>
+				
+				<p>&nbsp;<iframe width="80%" height="500" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2975.2047420454!2d-78.16259619999997!3d41.78080099999993!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89cdee29fbb82745%3A0x7df0b0f2b2cc1fc2!2s191+Main+St%2C+Roulette%2C+PA+16746!5e0!3m2!1sen!2sus!4v1409883424037" frameborder="0" style="border: 0px currentColor;"></iframe></p>
+				
+				</div>
+			</section>
+
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="copyright">
+					<p>© 2018 <a href="/">BG's Engine Clinic</a>. All Rights Reserved.<br><a href="https://www.shopify.com?utm_campaign=poweredby&amp;utm_medium=shopify&amp;utm_source=onlinestore" target="_blank" rel="nofollow">Powered by Shopify</a></p>
+				<li><a title="BG's Engine Clinic on Twitter" class="icon-social twitter ir" href="http://www.twitter.com/shopify">Twitter</a></li>
+          <li><a title="BG's Engine Clinic on Facebook" class="icon-social facebook ir" href="http://www.facebook.com/shopify">Facebook</a></li>
+          <li><a title="BG's Engine Clinic on YouTube" class="icon-social youtube ir" href="http://www.youtube.com/user/shopify">YouTube</a></li>
+          
+          <li><a title="BG's Engine Clinic on Instagram" class="icon-social instagram ir" href="http://instagram.com/shopify">Instagram</a></li>
+          <li><a title="BG's Engine Clinic on Pinterest" class="icon-social pinterest ir" href="http://pinterest.com/shopify/">Pinterest</a></li>
+          <li><a title="BG's Engine Clinic on Vimeo" class="icon-social vimeo ir" href="http://vimeo.com/shopify">Vimeo</a></li>
+          
+          <li><a title="BG's Engine Clinic on Google+" class="icon-social google ir" href="https://plus.google.com/+shopify">Google+</a></li>
+				</div>
+			</footer>
+			
+			
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
